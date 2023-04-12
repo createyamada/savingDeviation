b72e0d0c491f2c38daab432774fa41b14948dbb1
MarshallDTeech <lastauaustralo@gmail.com>
2023-04-12 12:40:55 +0900

    テスト

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,6 +2,7 @@
 echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
+composer update
 
 echo "Caching config..."
 php artisan config:cache
@@ -9,5 +10,7 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Running migrations..."
-php artisan migrate --force
+php artisan serve
+
+# echo "Running migrations..."
+# php artisan migrate --force
